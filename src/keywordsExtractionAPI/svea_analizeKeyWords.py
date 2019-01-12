740b071be841631e987f292050b00625b2fbeaad
Aaron Zhang <aaronzhang@gmail.com>
2019-01-12 14:45:34 +0900

    Add function of get POS words

--- src/keywordsExtractionAPI/svea_analizeKeyWords.py
+++ src/keywordsExtractionAPI/svea_analizeKeyWords.py
@@ -30,6 +30,9 @@ from svea_wikipediaManager import svea_wikipediaManager
 class KeyWords:
   modelTuple = ("en", "en_core_web_sm")
   
+  # POS words type list
+  POS_NOUN = "NOUN"
+  
   def __init__(self):
     print("***AZ[Init KeyWords class]")
     
@@ -39,6 +42,7 @@ class KeyWords:
     #doc = nlp(u"This is a sentence.")
     doc = nlp(_contentText)
     print([(w.text, w.pos_) for w in doc])
+    return doc
 
   def splitSentenceEnCoreWebSm(self, _contentText):
     nlp = spacy.load(self.modelTuple[1])
@@ -46,16 +50,37 @@ class KeyWords:
     print(doc.text)
     for token in doc:
       print(token.text, token.pos_, token.dep_)
-    
+    return doc
+
+  # Get word list by special word type
+  # POS: https://spacy.io/api/annotation
+  def getWordListOfSpecialWordType(self, _doc, _wordType):
+    _nounDic = {}
+    for token in _doc:
+      print(token.text, token.pos_, token.dep_)
+      if (token.pos_ == "NOUN"):
+        _nounDic[token.text] = token.pos_
+    return _nounDic
+
   # python -m spacy download en_core_web_sm
   def splitSentence(self, _modelType, _contentText):
+    doc = ""
     if _modelType == self.modelTuple[0]:
-      self.splitSentenceEn(_contentText)
+      doc = self.splitSentenceEn(_contentText)
     elif _modelType == self.modelTuple[1]:
       _contentText = sentences[0]
-      self.splitSentenceEnCoreWebSm(_contentText)
+      doc = self.splitSentenceEnCoreWebSm(_contentText)
     else:
       print("No model be choosed!")
+    return doc
+
+  # Analize file contents
+  def splitSentenceFromFile(self, _modelType, _textFile):
+    file = open(_textFile, "r")
+    _contentText = file.read()
+    doc = self.splitSentence(_modelType, _contentText)
+    return doc
+    
 
 def main(args):
     return 0
@@ -70,7 +95,13 @@ if __name__ == '__main__':
     #kw.splitSentence(_modelType=kw.modelTuple[1])
     
     # test 2 intergrate wikpedia
-    kw.splitSentence(_modelType=kw.modelTuple[0], _contentText=content)
+    #kw.splitSentence(_modelType=kw.modelTuple[0], _contentText=content)
     
+    # test 3 test file content
+    doc = kw.splitSentenceFromFile(_modelType=kw.modelTuple[0], _textFile="../../testData/textData1.txt")
     
+    # test 4: test word POS
+    nounDic = kw.getWordListOfSpecialWordType(_doc=doc, _wordType=kw.POS_NOUN)
+    print("=======================================")
+    print(nounDic)
     #sys.exit(main(sys.argv))
